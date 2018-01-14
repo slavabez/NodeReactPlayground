11dc169f986b7ed7c54f19774bd334bad89b7bb7
Slava Bezgachev <bezgachev@gmail.com>
2018-01-14 23:44:56 +0600

    Sorta working with a progress bar

--- client/src/components/UploadForm.js
+++ client/src/components/UploadForm.js
@@ -3,36 +3,57 @@ import axios from 'axios';
 
 export default class UploadForm extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            isProcessing: false,
-            fileNameSelected: 'Please select a file',
-            file: {}
+            isUploading: false,
+            currentProgress: 0,
+            maxProgress: null
         };
     }
 
-    handleFileSelected = async (e) => {
+    handleFileSelected = (e) => {
+        const _this = this;
         const reader = new FileReader();
         const file = e.target.files[0];
-        reader.onload = (e) => {
-            console.log(e.target);
+        reader.onload = async (e) => {
+            _this.setState({
+                isUploading: true
+            });
+            const config = {
+                onUploadProgress: progressEvent => {
+                    _this.setState({
+                        currentProgress: progressEvent.loaded,
+                        maxProgress: progressEvent.total
+                    });
+                }
+            };
+            const response = await axios.post('/api/file', e.target.result, config);
+            _this.setState({
+                isUploading: false
+            });
         };
 
         reader.readAsDataURL(file);
     };
 
-    render(){
-        return(
-                <div className="file-field input-field">
+    render() {
+
+        const progress = (this.state.isUploading) ?
+            <progress value={this.state.currentProgress} max={this.state.maxProgress}/> :
+            <span>Choose a file to upload</span>;
+
+        return (
+            <div className="file-field input-field">
+                {!this.state.isUploading ? (
                     <div className="btn">
                         <span>File</span>
-                        <input type="file" onChange={this.handleFileSelected} />
+                        <input type="file" onChange={this.handleFileSelected}/>
                     </div>
-                    <div className="file-path-wrapper">
-                        <input className="file-path validate" type="text" value={this.state.fileNameSelected} readOnly />
-                    </div>
-                </div>
+                ) : ('')}
+
+                {progress}
+            </div>
         );
     }
 }
