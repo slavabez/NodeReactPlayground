d58de559e7effd15af3bfcd04753772ca499dda6
Slava Bezgachev <bezgachev@gmail.com>
2018-02-25 21:24:45 +0600

    trying to get fullscreen to work

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,13 +5,18 @@ import YetBetterUploadForm from './YetBetterUploadForm';
 
 export default class App extends React.Component {
 
-    render(){
-        return (
-            <div className='container'>
-                <h3 style={{textAlign: 'center'}}>Multi upload form</h3>
-                <YetBetterUploadForm />
-            </div>
-        );
-    }
+  handleFullscreenClick = () => {
+
+  };
+
+  render() {
+    return (
+      <div className='container'>
+        <h3 style={{textAlign: 'center'}}>Multi upload form</h3>
+        <button onClick={}>Full screen</button>
+        <YetBetterUploadForm/>
+      </div>
+    );
+  }
 };
 
